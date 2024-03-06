9372e9f790026adcd7ff8a9e8a86411727bc636d
sirius <sirius2beta@gmail.com>
2024-03-06 20:15:08 +0800

    add device

---
+++ device.cpp
@@ -0,0 +1,89 @@
+﻿#include "device.h"
+
+Device::Device(): ID(-1),type(0)
+{
+    pinUIDList = {};
+}
+
+Device::Device(const Device &d)
+{
+    this->deviceName = d.deviceName;
+    this->ID = d.ID;
+    this->settings = d.settings;
+    this->type = d.type;
+    this->pinUIDList = d.pinUIDList;
+}
+
+int Device::addPin(int boardID, int pinID)
+{
+    //return UID
+    pinUIDList.append(boardID*256+pinID);
+    return boardID*256+pinID;
+}
+
+int Device::addPin(int UID)
+{
+    //return UID
+    pinUIDList.append(UID);
+    return UID;
+}
+
+
+bool Device::operator== (const Device& lDev)
+{
+    if(this->ID == lDev.ID){
+        return true;
+    }
+    return false;
+}
+
+Device::~Device()
+{
+    qDebug()<<"device deleted";
+}
+
+int Device::boardofPin(int index)
+{
+    if(index<pinUIDList.size()){
+        return pinUIDList[index]/256;
+    }else{
+        return -1;
+    }
+}
+
+int Device::pinIDofPin(int index)
+{
+    if(index<pinUIDList.size()){
+        return pinUIDList[index]%256;
+    }else{
+        return -1;
+    }
+}
+
+QString Device::typeName()
+{
+    if(this->type == DEV::SENSOR){
+        return "sensor";
+    }else if(this->type == DEV::SERVO){
+        return "servo";
+    }else if(this->type == DEV::STEPPER){
+        return "stepper";
+    }
+
+    return "unknown";
+
+}
+
+Peripheral::Peripheral(): ID(0), boardName(QString()), boardType(QString())
+{
+}
+
+Peripheral::Peripheral(const Peripheral &p)
+{
+    this->ID = p.ID;
+    this->boardName = p.boardName;
+    this->boardType = p.boardType;
+    this->pinSelected = p.pinSelected;
+}
+
+
---
+++ device.h
@@ -0,0 +1,53 @@
+﻿#ifndef DEVICE_H
+#define DEVICE_H
+#include <QWidget>
+#include <QDebug>
+
+class Component
+{
+public:
+    Component();
+    Component(const Component& c);
+    int periID;
+    int comID;
+    int type;
+    QList<int> pinList;
+    QString setting;
+
+};
+
+class Device
+{
+public:
+    Device();
+    Device(const Device &d);
+    ~Device();
+
+    int ID;
+    QString deviceName;
+    int type;
+    QString settings;
+    QList<int> pinUIDList;
+    int boardofPin(int index);
+    int pinIDofPin(int index);
+    int addPin(int boardID, int pinID);
+    int addPin(int UID);
+    QString typeName();
+    enum DEV {SENSOR = 0, SERVO = 1, STEPPER=2};
+    bool operator== (const Device& lDev);
+
+
+
+};
+
+class Peripheral{
+public:
+    Peripheral();
+    Peripheral(const Peripheral &p);
+    QList<int> pinSelected;
+    int ID;
+    QString boardName;
+    QString boardType;
+};
+
+#endif // DEVICE_H
